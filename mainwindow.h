c58742da5bbe2b2f9343b8e00c4f16d738e4f20a
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-07-23 18:11:43 +0300

    Added copy, paste, cut, undo, redo

--- mainwindow.h
+++ mainwindow.h
@@ -22,6 +22,27 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionPrint_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
 private:
     Ui::MainWindow *ui;
     QString current_file = "";
